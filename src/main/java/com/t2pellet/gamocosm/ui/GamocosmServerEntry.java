e636be7196800b6fcbfe4595442f8bda0de177d5
Tenzin Pelletier <pelletiertenzin2000@gmail.com>
2021-12-31 01:11:34 -0500

    Additional tweaks (improved the way the gamocosm entry looks in multiplayer screen)

--- src/main/java/com/t2pellet/gamocosm/ui/GamocosmServerEntry.java
+++ src/main/java/com/t2pellet/gamocosm/ui/GamocosmServerEntry.java
@@ -9,12 +9,13 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Util;
 
 //CommodoreThrawn: Add gamocosm server entry
 @Environment(EnvType.CLIENT)
 public class GamocosmServerEntry extends MultiplayerServerListWidget.Entry {
-    private static final Text TITLE_TEXT = new TranslatableText("gamocosm.server.title");
+
     private final MultiplayerScreen screen;
     protected final MinecraftClient client;
     protected final GamocosmServerInfo server;
@@ -27,8 +28,9 @@ public class GamocosmServerEntry extends MultiplayerServerListWidget.Entry {
     }
 
     public void render(MatrixStack matrices, int index, int y, int x, int entryWidth, int entryHeight, int mouseX, int mouseY, boolean hovered, float tickDelta) {
-        this.client.textRenderer.draw(matrices, this.server.getName(), (float)(x + 32 + 3), (float)(y + 1), 16777215);
-        this.client.textRenderer.draw(matrices, this.server.getAddress(), (float)(x + 32 + 3), (float)(y + 12), 8421504);
+        var title = Text.of(Formatting.BOLD + this.server.getName());
+        drawCenteredText(matrices, title, (float)(y + 5), 16777215);
+        drawCenteredText(matrices, Text.of(this.server.getAddress()), (float)(y + 20), 8421504);
     }
 
     public boolean mouseClicked(double mouseX, double mouseY, int button) {
@@ -46,7 +48,12 @@ public class GamocosmServerEntry extends MultiplayerServerListWidget.Entry {
     }
 
     public Text getNarration() {
-        return new TranslatableText("narrator.select", (new LiteralText("")).append(TITLE_TEXT).append(" ").append(this.server.getName()));
+        return new TranslatableText("narrator.select", (new LiteralText("")).append(server.getName()));
+    }
+
+    private void drawCenteredText(MatrixStack matrices, Text text, float y, int color) {
+        float centerX = (screen.width - this.client.textRenderer.getWidth(text)) / 2f;
+        this.client.textRenderer.draw(matrices, text, centerX, y, color);
     }
 }
 
